97c9a86746d68d52e172ce943673ae9afc7d3915
Nikita Pochaev <pochaev.nik@gmail.com>
2021-04-07 23:50:57 +0300

    Add Main class and FileEditor Control
    Signed-off-by: Nikita Pochaev <pochaev.nik@gmail.com>

---
+++ src/ru/etu/view/FileEditorControl.java
@@ -0,0 +1,72 @@
+package ru.etu.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.IndexRange;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.web.WebView;
+import org.fxmisc.richtext.CodeArea;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class FileEditorControl extends BorderPane {
+
+    @FXML
+    private CodeArea codeArea;
+    @FXML
+    private WebView webView;
+
+    // For traduction
+    @FXML
+    private Label selectedLineAndColLabel;
+
+    public FileEditorControl() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/fileEditorControl.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        setLocale(Locale.getDefault().toString());
+
+        codeArea.getStylesheets().add(MainApp.class.getResource("/style/codeAreaStyle.css").toExternalForm());
+
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+
+    public void resetSelection(IndexRange selectedText, int difference) {
+        codeArea.selectRange(selectedText.getStart() + difference, selectedText.getEnd() + difference);
+    }
+
+    public void resetCaretPosition(int caretPositon) {
+        codeArea.displaceCaret(caretPositon);
+    }
+
+    public IndexRange getSelection() {
+        return codeArea.getSelection();
+    }
+
+    public int getCurrentLine() {
+        return codeArea.getText(0, codeArea.getCaretPosition()).split("\n").length;
+    }
+
+    public int getCaretPosition() {
+        return codeArea.getCaretPosition();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale(lang);
+        ResourceBundle bundle = ResourceBundle.getBundle("config.lang", locale);
+    }
+}
