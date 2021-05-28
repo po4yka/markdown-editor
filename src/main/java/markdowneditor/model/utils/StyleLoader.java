2d1ad2575b49453c39c24c6df9b37c060fbafaf8
Nikita Pochaev <pochaev.nik@gmail.com>
2021-05-29 02:26:17 +0300

    Delete trash and update StylesLoader

--- src/main/java/markdowneditor/model/utils/StyleLoader.java
+++ src/main/java/markdowneditor/model/utils/StyleLoader.java
@@ -6,19 +6,31 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
 
 public abstract class StyleLoader {
     public static Map<String, String> getAllStyles() throws IOException {
         var map = new HashMap<String, String>();
-        URL pathUrl = StyleLoader.class.getResource("/stylesToLoad");
-        assert (pathUrl != null) : "pathUrl in StyleLoader is null";
-        var folder = new File(pathUrl.getPath());
-        var files = folder.listFiles();
-        assert (files != null) : "files in StyleLoader is null";
-        for (File file : files) {
-            var text = new String(Files.readAllBytes(file.toPath()));
-            map.put(file.getName().replaceAll(".css$", ""), text);
+
+        URL darkStylePath = StyleLoader.class.getResource("/stylesToLoad/Dark.css");
+        assert darkStylePath != null;
+        Scanner scannerDark = new Scanner(darkStylePath.openStream());
+        StringBuilder textDark = new StringBuilder();
+        while (scannerDark.hasNext()) {
+            textDark.append(scannerDark.next());
         }
+        map.put("Dark", textDark.toString());
+
+        URL defaultStylePath = StyleLoader.class.getResource("/stylesToLoad/Default.css");
+        assert defaultStylePath != null;
+        Scanner scannerDefault = new Scanner(defaultStylePath.openStream());
+        StringBuilder textDefault = new StringBuilder();
+        while (scannerDefault.hasNext()) {
+            textDefault.append(scannerDefault.next());
+        }
+        map.put("Default", textDefault.toString());
+
         return map;
     }
 }
